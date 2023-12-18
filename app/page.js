b530d64e3e6167814d04e1b363c72b8fccec7b7e
Shreyash Dubey <askshreyash@gmail.com>
2023-12-18 14:31:14 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+"use client"
+
+import { CardTitle, CardHeader, CardContent, Card } from "@components/ui/card"
+import { Button } from "@components/ui/button"
+import { useState } from 'react';
+
 
 export default function Home() {
+  const [showFAQ, setShowFAQ] = useState(true);
+
+  const toggleSection = () => {
+    setShowFAQ(!showFAQ);
+  };
+
+  const handleSendMessage = (message) => {
+    // Handle sending the message logic here
+    console.log(`Message sent: ${message}`);
+    // You can also toggle the section after sending the message if needed
+    toggleSection();
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="min-h-screen p-8">
+      <main>
+        
+        {showFAQ ? (
+          <section className="my-8">
+            <section className="my-8">
+          <h2 className="text-4xl text-center font-bold mb-4 orange_gradient">FAQs & Helpdesk</h2>
+          <h3 className="text-lg text-center mb-10">Know more about the life of chachaji and the project namami gange</h3>
+          <div className="grid grid-cols-2 gap-4">
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>What is the Ganga River's significance?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Learn about the Ganga's cultural importance and role as India's lifeline, sustaining ecosystems and communities.</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>What is the Namami Gange Mission?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Explore this government initiative's goals, focused on rejuvenating and conserving the Ganga River.</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>How does it promote sustainability?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Explore initiatives such as afforestation, biodiversity conservation, and eco-friendly practices.</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>How does Namami Gange address pollution?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Discover measures like wastewater treatment, solid waste management, and industrial effluent control.</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>How does Namami Gange address pollution?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Discover measures like wastewater treatment, solid waste management, and industrial effluent control.</p>
+              </CardContent>
+            </Card>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>How does Namami Gange address pollution?</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>Discover measures like wastewater treatment, solid waste management, and industrial effluent control.</p>
+              </CardContent>
+            </Card>
+          </div>
+        </section>
+
+
+        <section className="my-8">
+          <h2 className="text-xl font-semibold mb-4">Chat with Chacha Ji</h2>
+          <div className="bg-white p-6 rounded-lg shadow-md flex items-center space-x-4">
+            <input
+              aria-label="Chat input"
+              className="flex-grow border-2 border-gray-200 rounded-lg p-2"
+              placeholder="Type your message"
+              type="text"
             />
-          </a>
-        </div>
-      </div>
+            <Button className="ml-4">Send</Button>
+          </div>
+        </section>
+            <button className="flex-start p-3 max-w-[160px] my-6 border shadow-sm" onClick={toggleSection}>Switch to Chat</button>
+          </section>
+        ) : (
+              
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <section className="my-8">
+           
+           <h2 className="text-4xl text-center font-bold mb-4 orange_gradient">FAQs & Helpdesk</h2>
+           
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className=" p-6 rounded-lg border border-gray-300">
+            <div className="space-y-4">
+              <div className="flex items-end">
+                <div className="bg-orange-50 rounded-br-none rounded-lg p-2 text-gray-700">
+                  <p className="font-bold">You</p>
+                  <p>Hello, I need to know more about Ganga Ji.</p>
+                </div>
+              </div>
+              <div className="flex items-end justify-end">
+                <div className="bg-yellow-50 rounded-bl-none rounded-lg p-2 text-gray-700">
+                  <p className="font-bold">Chacha Chaudhary</p>
+                  <p>Ha bete kya janna chahte ho.</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center space-x-4 mt-4">
+              <input
+                aria-label="Chat input"
+                className="flex-grow border-2 border-gray-200 rounded-lg p-2"
+                placeholder="Type your message"
+                type="text"
+              />
+              <Button className="ml-4">Send</Button>
+            </div>
+          </div>
+          <button className="flex-start p-3 max-w-[160px] my-6 border shadow-sm" onClick={toggleSection}>Switch to FAQ</button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          </section>
+        )}
+      </main>
+      <footer className="py-4">
+        <p className="text-center text-gray-600">
+          Built by Team Sarvagya @ SIH 2023</p>
+      </footer>
+    </div>
   )
 }
